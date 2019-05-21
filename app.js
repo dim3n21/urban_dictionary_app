18f34ce2de7ef8cdd103c6920d603ae54124ba44
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-05-21 15:16:46 -0700

    Build the app with simple css and vanilla js

---
+++ app.js
@@ -0,0 +1,39 @@
+const form = document.querySelector('form');
+const input = document.querySelector('.form-group #search');
+const card = document.querySelector('.card-body');
+const cardHeader = document.querySelector('.card-title');
+const cardText = document.querySelector('.card-text');
+const readMoreButton = document.querySelector('.read-more-btn');
+
+const updateUI = (data) => {
+    card.classList.remove('d-none');
+    cardHeader.textContent = data.list[0].word;
+    cardText.textContent = data.list[0].definition;
+    readMoreButton.setAttribute('href', data.list[0].permalink);
+}
+
+
+// request
+const getData = async (request) => {
+    const response = await fetch(`http://api.urbandictionary.com/v0/define?term=${request}`)
+    const data = await response.json();
+    // console.log(data);
+    return data;
+}
+
+
+// Get the request from UI..
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let searchWord = input.value.trim();
+    form.reset();
+    getData(searchWord).then(data => updateUI(data));
+})
+
+
+
+
+
+
+
