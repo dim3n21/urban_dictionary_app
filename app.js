3502d14149f47e23b395d9e2249e62b00f3eb00a
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-05-22 11:42:00 -0700

    Build the 'history of search' in the project. Implement it with localStorage. Save and upload history after page refresh.

--- app.js
+++ app.js
@@ -4,12 +4,40 @@ const card = document.querySelector('.card-body');
 const cardHeader = document.querySelector('.card-title');
 const cardText = document.querySelector('.card-text');
 const readMoreButton = document.querySelector('.read-more-btn');
+const history = document.querySelector('.history-query');
+const clearHistory = document.querySelector('.clear-history-btn');
 
+let arrayOfSearch = [];
 
 const updateCopy = (copy) => {
     updatedDifinition = copy.split('[').join('').split(']').join('');
     cardText.textContent = updatedDifinition;
 };
+
+const restoreToSearchHistory = () => {
+    for (let key in localStorage) {
+        arrayOfSearch.push(key);
+    }
+
+    arrayOfSearch = arrayOfSearch.splice(0, localStorage.length);
+    
+    if (arrayOfSearch.length) {
+        arrayOfSearch.forEach( elem => {
+            history.innerHTML += `<div class="history-item">
+                              ${elem}
+                            </div>`
+        })
+    }
+}
+
+const addToSearchHistory = (search) => {
+    arrayOfSearch.push(search);
+    localStorage.setItem
+    localStorage.setItem(search.toString(), search);
+    history.innerHTML += `<div class="history-item">
+                              ${search}
+                            </div>`
+}
 
 const updateUI = (data) => {
     card.classList.remove('d-none');
@@ -17,6 +45,7 @@ const updateUI = (data) => {
     updateCopy(data.list[0].definition);
     //cardText.textContent = data.list[0].definition;
     readMoreButton.setAttribute('href', data.list[0].permalink);
+    addToSearchHistory(data.list[0].word);
 };
 
 
@@ -28,6 +57,9 @@ const getData = async (request) => {
     return data;
 }
 
+//restore history 
+window.addEventListener('load',restoreToSearchHistory());
+
 
 // Get the request from UI
 
@@ -36,6 +68,14 @@ form.addEventListener('submit', (e) => {
     let searchWord = input.value.trim();
     form.reset();
     getData(searchWord).then(data => updateUI(data));
+})
+
+// clear history of search
+
+clearHistory.addEventListener('click', () => {
+    localStorage.clear();
+    arrayOfSearch = [];
+    history.innerHTML = '';
 })
 
 
